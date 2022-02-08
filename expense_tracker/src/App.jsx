f94e0b92922dc4b043c12fa2d77693a440904434
admin <dave.rudschewski@gmx.de>
2022-02-08 15:15:49 +0100

    add EntryList Component

--- expense_tracker/src/App.jsx
+++ expense_tracker/src/App.jsx
@@ -1,10 +1,65 @@
+import React from 'react';
 import "./App.scss";
 import { useContext } from "react";
 import { ExpenseContext } from "./components/Context";
+import EntryList from "./components/EntryList";
+import { v4 as uuidv4 } from "uuid";
+import moment from "moment";
+import { useEffect } from 'react';
+
 function App() {
-	const { expense } = useContext(ExpenseContext);
+
+	const timing = moment().format('L');
+	const { setExpense, expense } = useContext(ExpenseContext);
+
+useEffect(() => {
+
+	setExpense( [
+		{
+		id: uuidv4(),
+		timing: timing,
+		category: "House",
+		amount: 800,
+		expenses: true,
+		},
+		{
+		id: uuidv4(),
+		timing: timing,
+		category: "Car",
+		amount: 200,
+		expenses: true,
+		},
+		{
+		id: uuidv4(),
+		timing: timing,
+		category: "Other",
+		amount: 250,
+		expenses: true,
+		},
+		{
+		id: uuidv4(),
+		timing: timing,
+		category: "Leisure Time",
+		amount: 100,
+		expenses: true,
+		},
+		{
+		id: uuidv4(),
+		timing: timing,
+		category: "Groceries",
+		amount: 300,
+		expenses: true,
+		},
+		{
+		id: uuidv4(),
+		timing: timing,
+		category: "Salary",
+		amount: 2500,
+		expenses: false,
+		}])}, [])
+
 	console.log("Expenses", expense);
-	return <div className='App'></div>;
+	return <div className='App'><EntryList /></div>;
 }
 
 export default App;
---
+++ expense_tracker/src/components/EntryList.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect, useContext, createContext } from 'react';
+import './EntryList.css';
+import { ExpenseContext } from "./Context";
+// import fakeData from './FakeData';
+import { VscEdit } from 'react-icons/vsc';
+import { FiDelete } from 'react-icons/fi';
+
+function EntryList() {
+  // const [expense, setExpense] = useState(initialExpenses);
+  const { setExpense, expense } = useContext(ExpenseContext);
+
+  const handleEdit = () => {
+    alert("Edit Item here..."); // for testing purpose
+  }
+
+  const handleDelete = () => {
+    alert("[x] - Item deleted."); // for testing purpose
+    //setExpense();
+  }
+
+  return (
+    <table>
+      {expense.reverse().map(({ id, timing, category, amount, expenses }) => (
+        <tr key={id}>
+          <td>{timing}</td>
+          <td className='left'>{category}</td>
+          <td className={expenses ? 'right expense' : 'right income'}>
+            {expenses ? '- ' : '+ '}{new Intl.NumberFormat('de-DE', {style: 'currency', currency: 'EUR'}).format(amount)}
+          </td>
+          <td>
+            <button onClick={handleEdit} className='btn-edit'><VscEdit /></button>
+            <button onClick={handleDelete} className='btn-delete'><FiDelete /></button>
+          </td>
+        </tr>
+      ))}
+    </table>
+  );
+}
+
+export default EntryList;
