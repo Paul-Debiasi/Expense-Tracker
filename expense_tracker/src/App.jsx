30e193485720989526352bb6fe15fffeb39a087a
kemdev <aa171@students.kiron.ngo>
2022-02-08 15:19:54 +0100

    almost finished my part

--- expense_tracker/src/App.jsx
+++ expense_tracker/src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.scss";
 import { useContext } from "react";
 import { ExpenseContext } from "./components/Context";
+import Overview from "./components/Overview";
+import ExpenseContextProvider from "./components/Context.jsx";
+import Test from './components/test'
+
+
 function App() {
-	const { expense } = useContext(ExpenseContext);
-	console.log("Expenses", expense);
-	return <div className='App'></div>;
+  const { expense } = useContext(ExpenseContext);
+//   console.log("Expenses", expense);
+  return (
+    <div className="App">
+	 
+      <Overview />
+      <ExpenseContextProvider />
+    </div>
+  );
 }
 
 export default App;
